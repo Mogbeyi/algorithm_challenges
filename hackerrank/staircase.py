5b4e8f09d394a1b4b506b66f72db6e08963a3a19
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2021-02-12 15:22:40 +0100

    Make minor changes to staircase program

--- hackerrank/staircase.py
+++ hackerrank/staircase.py
@@ -2,10 +2,10 @@ def staircase(n):
     space = n - 1
     star = 1
 
-    for i in range(n, 0, -1):
+    for i in range(1, n + 1):
         print(" " * space, star * "#", sep="")
         space -= 1
         star += 1
 
 
-staircase(20)
+staircase(7)
