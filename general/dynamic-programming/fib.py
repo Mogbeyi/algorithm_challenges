2f11a00b70a6bec5d104623b7f2905efd400df72
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2021-03-17 14:01:12 +0100

    Solve fibonacci problem using dynamic programming

--- general/dynamic-programming/fib.py
+++ general/dynamic-programming/fib.py
@@ -1,7 +1,12 @@
-def fib(n):
+def fib(n, cache={}):
+    if n in cache:
+        return cache[n]
     if n <= 2:
         return 1
-    return fib(n - 1) + fib(n - 2)
+    cache[n] = fib(n - 1) + fib(n - 2)
+    
+    return cache[n]
 
 print(fib(5))
 print(fib(8))
+print(fib(100))
