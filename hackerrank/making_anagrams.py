bf4b7ea6656e4a0d88dbd3be856e4a2a6c22f21c
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2021-03-30 21:32:58 +0100

    Update make anagrams solution

--- hackerrank/making_anagrams.py
+++ hackerrank/making_anagrams.py
@@ -1,34 +1,23 @@
-def build_hash_table(string):
-    hash_table = {}
+from collections import Counter
 
-    for char in string:
-        if char in hash_table:
-            hash_table[char] += 1
+def make_anagrams(s1, s2):
+    smaller_string = s1 if len(s1) <= len(s2) else s2
+    bigger_string = s2 if len(s2) >= len(s1) else s1
+    bigger_string_freq = Counter(bigger_string)
+    number_of_deletions = 0
+
+    for char in smaller_string:
+        if char in bigger_string_freq and bigger_string_freq[char] != 0:
+            bigger_string_freq[char] -= 1
         else:
-            hash_table[char] = 1
-
-    return hash_table
-
-def make_anagrams(first, second):
-    first_hash = build_hash_table(first)
-    second_hash = build_hash_table(second)
-    combined_string_length = len(first) + len(second)
-
-    for char in first_hash.keys():
-        if char in second_hash:
-            total = first_hash[char] + second_hash[char]
-            combined_string_length -= total 
+            number_of_deletions += 1
             
-    return combined_string_length
+    return number_of_deletions + sum(bigger_string_freq.values())
 
-print(make_anagrams('absdjkvuahdakejfnfauhdsaavasdlkj', 'djfladfhiawasdkjvalskufhafablsdkashlahdfa'))
-print(make_anagrams('abc', 'cde'))
+def main():
+   assert make_anagrams('abc', 'cde') == 4 
+   assert make_anagrams('abc', 'amnop') == 6 
+   assert make_anagrams('aaa', 'aaabb') == 2 
+   assert make_anagrams('absdjkvuahdakejfnfauhdsaavasdlkj', 'djfladfhiawasdkjvalskufhafablsdkashlahdfa') == 19
 
-#
-#def main():
-#    assert make_anagrams('abc', 'cde') == 4 
-#    assert make_anagrams('abc', 'amnop') == 6 
-#    assert make_anagrams('aaa', 'aaabb') == 2 
-#    assert make_anagrams('absdjkvuahdakejfnfauhdsaavasdlkj', 'djfladfhiawasdkjvalskufhafablsdkashlahdfa') == 4 
-#
-#main()
+main()
