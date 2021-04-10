ab6f7ad261709a7a7ae3c3d61907455c1458c9a4
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2021-04-10 15:04:51 +0100

    solve Movie flights problem

---
+++ interview-cake/hashtable/can-two-movies-fill-flight.py
@@ -0,0 +1,58 @@
+import unittest
+
+def can_two_movies_fill_flight(movie_lengths, flight_length):
+    if len(movie_lengths) < 2:
+        return False
+    
+    set_of_movie_lengths = set()
+
+    for elem in movie_lengths:
+        if flight_length - elem in set_of_movie_lengths:
+            return True
+        else:
+            set_of_movie_lengths.add(elem)
+
+    return False
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_short_flight(self):
+        result = can_two_movies_fill_flight([2, 4], 1)
+        self.assertFalse(result)
+
+    def test_long_flight(self):
+        result = can_two_movies_fill_flight([2, 4], 6)
+        self.assertTrue(result)
+
+    def test_one_movie_half_flight_length(self):
+        result = can_two_movies_fill_flight([3, 8], 6)
+        self.assertFalse(result)
+
+    def test_two_movies_half_flight_length(self):
+        result = can_two_movies_fill_flight([3, 8, 3], 6)
+        self.assertTrue(result)
+
+    def test_lots_of_possible_pairs(self):
+        result = can_two_movies_fill_flight([1, 2, 3, 4, 5, 6], 7)
+        self.assertTrue(result)
+
+    def test_not_using_first_movie(self):
+        result = can_two_movies_fill_flight([4, 3, 2], 5)
+        self.assertTrue(result)
+
+    def test_multiple_movies_shorter_than_flight(self):
+        result = can_two_movies_fill_flight([5, 6, 7, 8], 9)
+        self.assertFalse(result)
+
+    def test_only_one_movie(self):
+        result = can_two_movies_fill_flight([6], 6)
+        self.assertFalse(result)
+
+    def test_no_movies(self):
+        result = can_two_movies_fill_flight([], 2)
+        self.assertFalse(result)
+
+
+unittest.main(verbosity=2)
