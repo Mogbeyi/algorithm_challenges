9b152f69c1ba0da7d726358cd88a8bfe4cf3be4c
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2021-04-11 12:26:05 +0100

    Solve world-cloud-data challenge

---
+++ interview-cake/hashtable/world-cloud-data.py
@@ -0,0 +1,31 @@
+import unittest
+import re
+
+class WordCloudData(object):
+
+    def __init__(self, input_string):
+        self.words_to_counts = {}
+        self.input_string = input_string
+        self.split_string = self.input_string.lower().split()
+
+    def count_words(self):
+        for char in self.split_string:
+            char = re.sub(r'[^\w\s]', '', char)
+            if len(char) == 1 and char.isalpha == False:
+                continue
+            if char in self.words_to_counts:
+                self.words_to_counts[char] += 1
+            else:
+                self.words_to_counts[char] = 1
+
+        return self.words_to_counts
+
+    def print_result(self):
+        print(self.count_words())
+
+first = WordCloudData('I like cake')
+first.print_result()
+second = WordCloudData('Dessert - mille-feuille cake')
+second.print_result()
+third = WordCloudData("Allie's Bakery: Sasha's... Cakes")
+third.print_result()
