74d64021692e1afc43f2f9b7c1559935912846a8
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2021-04-13 02:47:06 +0100

    Perfectly solve merge list challenge

--- interview-cake/array/merge-lists.py
+++ interview-cake/array/merge-lists.py
@@ -2,78 +2,58 @@ import unittest
 
 
 def merge_lists(my_list, alices_list):
-    '''
-    my_list     = [2, 4, 6, 8]
-    alices_list = [1, 7]
-    my_list_pointer = 4
-    alices_list_pointer = 2
-    merged_list = [1, 2, 4, 6, 7,8] 
+    my_list_pointer = 0
+    alices_list_pointer = 0
+    my_list_length = len(my_list)
+    alices_list_length = len(alices_list)
+    merged_result = []
 
-    my_list [1, 7]
-    alices_list [2, 4]
-    '''
-    
-    my_list_pointer = 0     #1
-    alices_list_pointer = 0 #2
-    my_list_length = len(my_list) #2
-    alices_list_length = len(alices_list)  #2
-    bigger_list_length = my_list_length if my_list_length > alices_list_length else alices_list_length #2
-    merged_list = []  # [1, 2, 4]
+    while my_list_pointer < my_list_length or alices_list_pointer < alices_list_length:
+        if my_list_pointer == my_list_length:
+            merged_result += alices_list[alices_list_pointer:]
+            break
+        elif alices_list_pointer == alices_list_length:
+            merged_result += my_list[my_list_pointer:]
+            break
 
+        if my_list[my_list_pointer] <= alices_list[alices_list_pointer]:
+            first_result = my_list[my_list_pointer]
+            merged_result.append(first_result)
+            my_list_pointer += 1
+        elif alices_list[alices_list_pointer] < my_list[my_list_pointer]:
+            second_result = alices_list[alices_list_pointer]
+            merged_result.append(second_result)
+            alices_list_pointer += 1
 
-    while (my_list_pointer < bigger_list_length) or (alices_list_pointer < bigger_list_length):
-            print(merged_list, alices_list_pointer)
-            if my_list[my_list_pointer] < alices_list[alices_list_pointer] and my_list_pointer < my_list_length:
-                result = my_list[my_list_pointer]
-                merged_list.append(result)
-                if my_list_pointer != my_list_length:
-                    my_list_pointer += 1
-            elif my_list[my_list_pointer] > alices_list[alices_list_pointer] and alices_list_pointer < alices_list_length:
-                second_result = alices_list[alices_list_pointer]
-                merged_list.append(second_result)
-                if alices_list_pointer != alices_list_length:
-                    alices_list_pointer += 1
-            else:
-                my_result = my_list[my_list_pointer]
-                merged_list.append(my_result)
-                alice_result = alices_list[alices_list_pointer]
-                merged_list.append(alice_result)
-                my_list_pointer += 1
-                alices_list_pointer += 1
-    return merged_list
-
-# print(merge_lists([2, 4, 6, 8], [1, 2, 3, 7]))
-print(merge_lists([1, 7], [2, 4]))
+    return merged_result
 
 
-# Tests
+class Test(unittest.TestCase):
 
-# class Test(unittest.TestCase):
+    def test_both_lists_are_empty(self):
+        actual = merge_lists([], [])
+        expected = []
+        self.assertEqual(actual, expected)
 
-#     def test_both_lists_are_empty(self):
-#         actual = merge_lists([], [])
-#         expected = []
-#         self.assertEqual(actual, expected)
+    def test_first_list_is_empty(self):
+        actual = merge_lists([], [1, 2, 3])
+        expected = [1, 2, 3]
+        self.assertEqual(actual, expected)
 
-#     def test_first_list_is_empty(self):
-#         actual = merge_lists([], [1, 2, 3])
-#         expected = [1, 2, 3]
-#         self.assertEqual(actual, expected)
+    def test_second_list_is_empty(self):
+        actual = merge_lists([5, 6, 7], [])
+        expected = [5, 6, 7]
+        self.assertEqual(actual, expected)
 
-#     def test_second_list_is_empty(self):
-#         actual = merge_lists([5, 6, 7], [])
-#         expected = [5, 6, 7]
-#         self.assertEqual(actual, expected)
+    def test_both_lists_have_some_numbers(self):
+        actual = merge_lists([2, 4, 6], [1, 3, 7])
+        expected = [1, 2, 3, 4, 6, 7]
+        self.assertEqual(actual, expected)
 
-#     def test_both_lists_have_some_numbers(self):
-#         actual = merge_lists([2, 4, 6], [1, 3, 7])
-#         expected = [1, 2, 3, 4, 6, 7]
-#         self.assertEqual(actual, expected)
-
-#     def test_lists_are_different_lengths(self):
-#         actual = merge_lists([2, 4, 6, 8], [1, 7])
-#         expected = [1, 2, 4, 6, 7, 8]
-#         self.assertEqual(actual, expected)
+    def test_lists_are_different_lengths(self):
+        actual = merge_lists([2, 4, 6, 8], [1, 7])
+        expected = [1, 2, 4, 6, 7, 8]
+        self.assertEqual(actual, expected)
 
 
-# unittest.main(verbosity=2)
+unittest.main(verbosity=2)
