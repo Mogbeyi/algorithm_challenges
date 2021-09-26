d4749453f46d50e827d0f07cccdf76c64d686179
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2021-09-26 19:31:32 +0100

    Reverse a string using recursion

--- recursion/recursive-max.py
+++ recursion/recursive-max.py
@@ -15,7 +15,8 @@ max = arr[0] > arr[1] ? arr[0] : arr[1]
 def recursive_max(arr):
     if len(arr) == 1:
         return arr[0]
+    
     return arr[0] if arr[0] > recursive_max(arr[1:]) else recursive_max(arr[1:])
 
 print(recursive_max([1, 3, 4, -99, 10000, -3, 98]))
-print(recursive_max([1, 3, 4, -99, 10, -3, 98]))
+# print(recursive_max([1, 3, 4, -99, 10, -3, 98]))
---
+++ recursion/reverse_string.py
@@ -0,0 +1,5 @@
+def reverse_string(string):
+    if len(string) == 0:
+        return ''
+    
+    return string[-1] + reverse_string(string[:-1])
