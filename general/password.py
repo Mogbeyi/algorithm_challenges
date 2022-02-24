4548f359d46ce16476a7b47e1e7246e40d5350e3
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2022-02-24 20:02:45 +0100

    Solve password problem using currying

--- general/password.py
+++ general/password.py
@@ -9,26 +9,53 @@ def get_valid_passwords(data):
 
     return valid_passwords 
 
+# def is_valid(password):
+#     upper_letters = list(string.ascii_uppercase)
+#     lower_letters = list(string.ascii_lowercase)
+#     numbers = list('0123456789')
+#     chars = list('$#@')
+
+#     return has_char(upper_letters, password) and has_char(lower_letters, password) and has_char(numbers, password) and has_char(chars, password)
+
+# def has_char(chars, password):
+#     if ' ' in password:
+#         return False
+
+#     exist = False
+
+#     for char in chars:
+#        if char in password:
+#             exist = True
+#             break
+
+#     return exist
+
 def is_valid(password):
-    upper_letters = list(string.ascii_uppercase)
-    lower_letters = list(string.ascii_lowercase)
-    numbers = list('0123456789')
-    chars = list('$#@')
+    contains_characters = has_char(password)
+    contains_upper = contains_characters(string.ascii_uppercase)
+    contains_lower = contains_characters(string.ascii_lowercase)
+    contains_numbers = contains_characters(list('0123456789'))
+    contains_symbols = contains_characters(list('$#@'))
 
-    return has_char(upper_letters, password) and has_char(lower_letters, password) and has_char(numbers, password) and has_char(chars, password)
+    return contains_upper and contains_lower and contains_numbers and contains_symbols
 
-def has_char(chars, password):
-    if ' ' in password:
-        return False
+def has_char(password):
+    def contains_char(chars): 
+        if ' ' in password:
+            return False
 
-    exist = False
+        exist = False
 
-    for char in chars:
-       if char in password:
-            exist = True
-            break
+        for char in chars:
+           if char in password:
+                exist = True
+                break
+        return exist
 
-    return exist
+    return contains_char
+
+
+
 
 def main():
     data = input("Enter passwords separated by comma: ").split(',')
