005cf66e3f9b195f5a4f81c15da973c10bbfe600
Mogbeyi <mogbeyiwenyinmi@gmail.com>
2023-03-09 23:11:53 +0100

    Make minor changes

--- recursion/palindrome.py
+++ recursion/palindrome.py
@@ -1,9 +1,11 @@
 def is_palindrome(string):
-    if len(string) == 0 or len(string) == 1:
+    if not len(string) or len(string) == 1:
         return True
     elif string[0] != string[-1]:
         return False 
     else:
+        print(string)
         return is_palindrome(string[1:-1])
 
 print(is_palindrome("eve"))
+print(is_palindrome("emme"))
